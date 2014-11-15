770a519732f67fb1980fa370d011219c3a064122
HugoCapitan <hmoravilez@gmail.com>
2014-11-14 22:20:44 -0600

    Proyecto sfotipy

---
+++ sfotipy/tracks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from artists.models import Artist
+from albums.models import Album
+
+
+class Track(models.Model):
+	title = models.CharField(max_length=255)
+	order = models.PositiveIntegerField()
+	track_file = models.FileField(upload_to='tracks')
+	artist = models.ForeignKey(Artist)
+	album = models.ForeignKey(Album)
+
+	def __unicode__(self):
+		return self.title
